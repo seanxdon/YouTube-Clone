2f25032e259a208f15c5e3ff5f6753a8d0d9e659
Sean <seanhb714@gmail.com>
2020-12-16 13:30:56 -0800

    Default Mount Component

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import VideoList from './VideoList';
 
 const VideoDetail = ({ video }) => {
 	if (!video) {
 		return <div>Search from a Video</div>;
 	}
-	return <div>{video.snippet.title}</div>;
+
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+	return (
+		<div>
+			<div className="ui embed">
+				<iframe title="Video Player" src={videoSrc}></iframe>
+			</div>
+			<div className="ui segment">
+				<h4 className="ui header">{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default VideoDetail;
